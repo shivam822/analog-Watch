ccebac2f36549b88b3e86ee3d646bae3d5dda82b
shivam kumar <69735374+shivam822@users.noreply.github.com>
2023-07-31 17:43:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-//  let hr= document.getElementById('Hrs');
-//  let min= document.getElementById('Min');
-//  let sec= document.getElementById('Sec');
-
-//  function displayTime() {
-//     let date= new Date();
-    
-//     let hh= date.getHours();
-//     let mm = date.getMinutes();
-//     let ss = date.getSeconds();
-
-//     let hRotation= 30*hh + mm/2;
-//     let mRotation= 6*mm;
-//     let sRotation=6*ss;
-
-//     hr.style.transform = `rotate (${hRotation}deg)`;
-//     min.style.transform = `rotate(${mRotation}deg)`;
-//     sec.style.transform = `rotate (${sRotation}deg)`;
-
-//  }
-// setInterval(displayTime, 1000);
-
 setInterval(() => {
     let date= new Date();
     
